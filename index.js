cbe2d1d1d17f5bb3a4bcd4c62d1a50e5ac544c3f
Sax Man <ohmyshoulder987@gmail.com>
2019-04-11 22:46:47 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const happyDay = new Date("October 28, 2019 00:00:00").getTime();
+
+setInterval(() => {
+    let now = new Date().getTime();
+    let daysLeft = happyDay - now;
+
+    let d = Math.floor(daysLeft / (1000 * 60 * 60 * 24));
+    let h = Math.floor((daysLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let m = Math.floor((daysLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let s = Math.floor((daysLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('inner').innerHTML = `${d} : ${h} : ${m} : ${s}`;
+}, 1000);
